3ac4329fc01f0db4e93b296c40588ef23e5ebe57
Max Hora <maxhora.ua@gmail.com>
2021-08-23 19:58:13 +0300

    Conan recipe.

---
+++ conanfile.py
@@ -0,0 +1,34 @@
+from conans import ConanFile, CMake
+
+
+class GigamonkeyConan(ConanFile):
+    name = "gigamonkey"
+    version = "0.1"
+    license = "<Put the package license here>"
+    author = "<Put your name here> <And your email here>"
+    url = "<Package recipe repository url here, for issues about the package>"
+    description = "<Description of Gigamonkey here>"
+    topics = ("<Put some tag here>", "<here>", "<and here>")
+    settings = "os", "compiler", "build_type", "arch"
+    options = {"shared": [True, False], "fPIC": [True, False]}
+    default_options = {"shared": False, "fPIC": True}
+    generators = "cmake_find_package"
+    exports_sources = "*"
+    requires = "boost/1.76.0", "openssl/1.1.1k", "cryptopp/8.5.0", "nlohmann_json/3.10.0", "SECP256K1/0.1", "gmp/6.2.1", "data/0.1"
+
+    def config_options(self):
+        if self.settings.os == "Windows":
+            del self.options.fPIC
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def package(self):
+        self.copy("*.h", dst="include", src="include")
+        self.copy("*.hpp", dst="include", src="include")
+        self.copy("libgigamonkey.a", dst="lib", keep_path=False)
+
+    def package_info(self):
+        self.cpp_info.libs = ["gigamonkey"]
